bcc9bc13c0e8a89e24fb4a52398723cbc8cba865
Neil Crawford <neil.crawford@gmail.com>
2015-04-20 12:54:11 +0100

    More modules and a build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
 var gulp = require('gulp');
+var Metalsmith = require('metalsmith');
+var markdown = require('metalsmith-markdown');
+var templates = require('metalsmith-templates');
+var handlebars = require('handlebars');
 
 
 
 gulp.task('default', function(done){
   console.log("test");
   done();
+});
+
+gulp.task('build', function(done){
+
+  Metalsmith(__dirname)
+    .use(markdown())
+    .use(templates('handlebars'))
+    .build(function(err) {
+      done(err);
+    });
+
 });
