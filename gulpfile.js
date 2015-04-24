19844347a5d8b0bbadeaa1568cec5fe7410bb797
Neil Crawford <neil.crawford@gmail.com>
2015-04-24 10:28:00 +0100

    Making things more bloggy

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,39 @@ var gulp = require('gulp');
 var Metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
 var templates = require('metalsmith-templates');
-var handlebars = require('handlebars');
+var collections = require('metalsmith-collections');
+var permalinks  = require('metalsmith-permalinks');
+var Handlebars = require('handlebars');
 var metadata = require("./metadata.json");
+
+
+Handlebars.registerPartial('header', 
+  fs.readFileSync(__dirname + '/templates/partials/header.hbt').toString()
+  );
+Handlebars.registerPartial('footer', 
+  fs.readFileSync(__dirname + '/templates/partials/footer.hbt').toString()
+  );
 
 gulp.task('default', [ 'build-html' ]);
 
 gulp.task('build-html', function(done){
 
   Metalsmith("./")
-    //.source("./src")
     .metadata( metadata )
-    //.directory("./build")
+    .use(collections({
+      pages: {
+        pattern: 'content/pages/*.md'
+      },
+      posts: {
+        pattern: 'content/posts/*.md',
+        sortBy: 'date',
+        reverse: true
+      }
+    }))
     .use(markdown())
+    .use(permalinks({
+      pattern: ':collection/:title'
+    }))
     .use(templates({
       "engine": "handlebars"
     }))
---
+++ src/content/posts/first-post.md
@@ -0,0 +1,7 @@
+---
+title: My First Post
+date: 2015-04-23
+template: post.hbt
+---
+
+My amazing first post
